26dcf9e516cdb975e45e0c9e5e515c31a07c1a79
PrasadHendry <prasad.tambe247@gmail.com>
2025-12-12 12:33:21 +0530

    ..

--- Backend/Awr.Worker/Configuration/WorkerConstants.cs
+++ Backend/Awr.Worker/Configuration/WorkerConstants.cs
@@ -2,10 +2,18 @@
 {
     public static class WorkerConstants
     {
+        /*
         // Paths
+        QA-24
         public const string SourceRoot = @"C:\AwrTest\AWR Issuance"; // AWR - DOC source folder
         public const string TempLocation = @"C:\AwrTest\Working"; // TEMP working - folder for process isolation
         public const string FinalLocation = @"C:\AwrTest\FinalIssuedDocs"; // Where QA generated docs live
+        */
+
+        public const string SourceRoot = @"\\192.15.15.100\AWR Issuance"; // AWR - DOC source folder
+        public const string TempLocation = @"\\192.15.15.100\AWR Issuance\Working"; // TEMP working - folder for process isolation
+        public const string FinalLocation = @"\\192.15.15.100\AWR Request\FinalIssuedDocs"; // Where QA generated docs live
+
 
         // Security
         public const string EncryptionPassword = "QA";
