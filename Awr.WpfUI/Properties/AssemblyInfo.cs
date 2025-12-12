464ccda0973e29d444e5109771169736551fcd52
PrasadHendry <prasad.tambe247@gmail.com>
2025-12-12 13:04:01 +0530

    .

--- Awr.WpfUI/Properties/AssemblyInfo.cs
+++ Awr.WpfUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Awr.WpfUI")]
+[assembly: AssemblyTitle("AWR Issuance System")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Awr.WpfUI")]
+[assembly: AssemblyProduct("AWR Issuance System")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
